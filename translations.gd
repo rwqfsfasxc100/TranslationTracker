6c43002381cedfd51686870fd0b901ac63505dc4
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-01-30 12:01:41 -0800

    updated tool

---
+++ translations.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var locales = [ "cs_CZ", "de", "el_GR", "en", "es", "fr", "hu_HU", "it_IT", "ja", "ko_KR", "nb_NO", "nl_NL", "pl", "pt_BR", "ru_RU", "th", "uk_UA", "zh_CN", "zh_HK" ]
+var default = "en"
+
+func _ready():
+	pass
